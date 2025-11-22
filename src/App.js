001efc4fac7c97725775faff9459432b072f5a70
joeys7585 <joecasillas001@gmail.com>
2025-11-22 13:27:49 +0530

    Fix build errors: restore missing imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import React from "react";
+import AboutMe from "./components/AboutMe";
+import Social from "./components/Social";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import MatrixBackground from "./components/MatrixBackground";
+import Cursor from "./components/Cursor";
+import "./App.css";
+
 function App() {
 	return (
 		<div className="App">
